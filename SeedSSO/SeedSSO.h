0eba0a5a5efd1aa7e03c31f03b4b724004f52afb
OliHire-HellowJingQiu <hellowjingqiu@CongYi-JokerV.local>
2015-05-05 11:39:34 +0800

    first

---
+++ SeedSSO/SeedSSO.h
@@ -0,0 +1,19 @@
+//
+//  SeedSSO.h
+//  SeedSSO
+//
+//  Created by OliHire-HellowJingQiu on 15/5/5.
+//  Copyright (c) 2015年 OliHire-JokerV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SeedSSO.
+FOUNDATION_EXPORT double SeedSSOVersionNumber;
+
+//! Project version string for SeedSSO.
+FOUNDATION_EXPORT const unsigned char SeedSSOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeedSSO/PublicHeader.h>
+
+
